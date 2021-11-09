c0e33dfc459db65e06a4b414be5a990ea2780f14
Craig Drabik <craig.drabik@txmq.com>
2021-11-09 13:55:56 -0500

    Added Winston for logging

--- src/ioc/ioc.ts
+++ src/ioc/ioc.ts
@@ -3,10 +3,35 @@ import { Logger } from '../helpers/logger';
 import { TYPES } from './types';
 import { HelloWorldController } from '../api/helloworld/controller';
 import { ApiServer } from '../apiserver';
+import * as Winston from 'winston';
 
 const container = new Container();
+// Set up logging through Winston
+container.bind<Logger>(TYPES.Logger).toDynamicValue(
+    () => {
+        const consoleTransport = new Winston.transports.Console({
+            format: Winston.format.combine(
+              Winston.format.colorize(),
+              Winston.format.timestamp(),
+              Winston.format.align(),
+              Winston.format.printf(info => {
+                const { timestamp, level, message, ...args } = info;
+      
+                const ts = timestamp.slice(0, 19).replace('T', ' ');
+                return `${ts} [${level}]: ${message} ${
+                  Object.keys(args).length ? JSON.stringify(args, null, 2) : ''
+                }`;
+              })
+            ),
+            level: process.env.LOG_LEVEL,
+          });
+      
+          return Winston.createLogger({
+            transports: [consoleTransport],
+          });
+    }
+).inSingletonScope();
 
-container.bind<Logger>(TYPES.Logger).to(Logger).inSingletonScope();
 container.bind<ApiServer>(TYPES.ApiServer).to(ApiServer).inSingletonScope();
 container.bind<HelloWorldController>(TYPES.HelloWorldController).to(HelloWorldController).inSingletonScope();
 
