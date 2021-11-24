ab43c68b33f9df5c2ad54927b836039899b8483d
Loi Tra 9thwonder <trathailoi@gmail.com>
2021-11-24 16:38:45 +0700

    Update apiserver.ts

--- src/apiserver.ts
+++ src/apiserver.ts
@@ -21,7 +21,23 @@ class ApiServer {
         this.hapiServer = new Server({
             port: configue.get('hapi.port', 8080),
             routes: {
-                cors: true
+              cors: true,
+              validate: {
+                failAction: async (request, h, err) => {
+                    if (process.env.NODE_ENV === 'production') {
+                        // In prod, log a limited error message and throw the default Bad Request error.
+                        // console.error('ValidationError:', err?.message) // Better to use an actual logger here.
+                        this.logger.info('ValidationError:', err)
+                        throw Boom.badRequest(`Invalid request payload input`)
+                    } else {
+                        if (process.env.NODE_ENV !== 'test') {
+                            // During development, log and respond with the full error.
+                            console.error(err)
+                        }
+                        throw err
+                    }
+                }
+              }
             }
         });
         this.hapiServer.validator(require('@hapi/joi'));
